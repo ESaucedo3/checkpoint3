1b0b6ad29af6c24593ed97c8d733cc81b1d142a0
Eduardo Sosa-Saucedo <117676083+ESaucedo3@users.noreply.github.com>
2024-08-30 13:31:01 -0600

    created necessary templates

--- app/AppState.js
+++ app/AppState.js
@@ -4,9 +4,9 @@ import {createObservableProxy} from './utils/ObservableProxy.js';
 
 class ObservableAppState extends EventEmitter {
   notes = [
-    new Note({title: 'CSS Tricks', color: '#333', body: 'CSS is very cool although there are times it can be headache to work with mostly height & images'}),
-    new Note({title: 'JS Tricks', color: '#333', body: 'You foreach to access items within array and so much more'}),
-    new Note({title: 'C# Stuff', color: '#333', body: 'Not sure what to say yet'}),
+    new Note({title: 'CSS Tricks', color: '#00008B', body: 'CSS is very cool although there are times it can be headache to work with mostly height & images'}),
+    new Note({title: 'JS Tricks', color: '#9F2B68', body: 'You foreach to access items within array and so much more'}),
+    new Note({title: 'C# Stuff', color: '#8B0000', body: 'Not sure what to say yet'}),
   ];
 }
 
--- app/models/Note.js
+++ app/models/Note.js
@@ -12,7 +12,7 @@ export class Note {
   }
 
   get NotesTemplate() {
-    return `<div class="border-start border-2 border-light p-2 mb-2">
+    return `<div class="note-style border-2 p-2 mb-2" style="border-color: ${this.color};">
                   <div class="row align-items-center">
                     <div class="col-6">
                       <h4>${this.title}</h4>
@@ -27,7 +27,27 @@ export class Note {
                 </div>`;
   }
 
-  get ActiveNoteTemplate() {}
+  get ActiveNoteTemplate() {
+    return `<div class="row justify-content-center">
+                <div class="col-10 py-3">
+                  <div class="row text-light">
+                    <div class="col-6 position-relative">
+                      <i class="fa-solid fa-bookmark fa-2xl position-absolute bookmark" style="color: #63e6be"></i>
+                      <h4>CSS Tricks</h4>
+                      <p class="m-0">Created on: 06/04/23</p>
+                      <p>Last Updated on: Wednesday 6/8/23 10:43am</p>
+                    </div>
+
+                    <div class="col-6 d-flex justify-content-end align-items-end mb-2">
+                      <button class="btn btn-outline-danger rounded px-2 align-self-end"><i class="fa-solid fa-trash" style="color: #d70000"></i> Delete</button>
+                      <button class="btn btn-outline-primary rounded px-2 align-self-end ms-3"><i class="fa-solid fa-floppy-disk" style="color: #74c0fc"></i> Save</button>
+                    </div>
+
+                    <textarea class="form-control border border-2 border-primary bg-dark text-light" name="body" rows="19"></textarea>
+                  </div>
+                </div>
+              </div>`;
+  }
 
   get NoActiveNoteTemplate() {
     return `<div class="position-absolute top-0 start-0 w-100 h-100 d-flex flex-column justify-content-center             align-items-center">
